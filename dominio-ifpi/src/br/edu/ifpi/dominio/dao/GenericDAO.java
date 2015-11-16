3b98621aaec8686b6cf66829f8d853a386a4d247
João Victor <joao.araujo.367@ig.com.br>
2015-11-16 09:59:36 -0300

    Adicionando pacote dao com uma classe generica

---
+++ dominio-ifpi/src/br/edu/ifpi/dominio/dao/GenericDAO.java
@@ -0,0 +1,13 @@
+package br.edu.ifpi.dominio.dao;
+
+import java.util.List;
+
+public interface GenericDAO<Entity> {
+
+	public void inserir(Entity e);
+	public void	remover(Entity e);
+	public void atualizar(Entity e);
+	public Entity pesquisar(Entity e, String campo);
+	public List<Entity> pesquisar(String jql);
+	
+}
