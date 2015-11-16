58e078d8d93a78616c997b96b948a25adea98fa7
João Victor <joao.araujo.367@ig.com.br>
2015-11-16 19:03:22 -0300

    Mudancas na classe GenericDAO

--- dominio-ifpi/src/br/edu/ifpi/dominio/dao/GenericDAO.java
+++ dominio-ifpi/src/br/edu/ifpi/dominio/dao/GenericDAO.java
@@ -7,7 +7,7 @@ public interface GenericDAO<Entity> {
 	public void inserir(Entity e);
 	public void	remover(Entity e);
 	public void atualizar(Entity e);
-	public Entity pesquisar(Entity e, String campo);
+	public Entity pesquisar(long id);
 	public List<Entity> pesquisar(String jql);
 	
 }
