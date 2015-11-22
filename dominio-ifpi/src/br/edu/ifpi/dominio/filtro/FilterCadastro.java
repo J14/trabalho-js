52e0c64162879b840d038692a8d731aae0962e85
João Victor <joao.araujo.367@ig.com.br>
2015-11-22 15:27:36 -0300

    Mudanca no filtro

--- dominio-ifpi/src/br/edu/ifpi/dominio/filtro/FilterCadastro.java
+++ dominio-ifpi/src/br/edu/ifpi/dominio/filtro/FilterCadastro.java
@@ -2,12 +2,16 @@ package br.edu.ifpi.dominio.filtro;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.edu.ifpi.dominio.dao.UsuarioDAO;
 import br.edu.ifpi.dominio.model.Usuario;
@@ -36,22 +40,26 @@ public class FilterCadastro implements Filter {
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		
-		
-		
+		HttpSession session = ((HttpServletRequest) request).getSession();
 		
 		String ident = request.getParameter("ident");
 		String senha = request.getParameter("senha");
 		
-		Usuario u = new UsuarioDAO().isCadastrado(ident, senha);
+		EntityManager em = (EntityManager) request.getServletContext().getAttribute("em");
+		
+		Usuario u = new UsuarioDAO(em).isCadastrado(ident, senha);
 		
 		
 		if (u == null) {			
-			request.setAttribute("msg-login", true);
-			request.getRequestDispatcher("index.jsp").forward(request, response);
+			/*request.setAttribute("msg-login", true);
+			request.getRequestDispatcher("index.jsp").forward(request, response);*/
+			session.setAttribute("msg-login", true);
+			((HttpServletResponse) response).sendRedirect("/dominio-ifpi");
 		} else {			
 			request.setAttribute("usuario", u);			
 			chain.doFilter(request, response);
 		}
+		
 	}
 
 	/**
